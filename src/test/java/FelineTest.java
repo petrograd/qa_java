2ed33d47fd623803c054b0465c7a034b061b90b4
Petr Merkurev <9662196@mail.ru>
2022-08-31 16:46:10 +0300

    first test done

--- src/test/java/FelineTest.java
+++ src/test/java/FelineTest.java
@@ -1,2 +1,26 @@
+import com.example.Feline;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.junit.MockitoJUnitRunner;
+
+import java.util.List;
+
+import static org.junit.Assert.assertEquals;
+
+@RunWith(MockitoJUnitRunner.class)
 public class FelineTest {
+
+    @Mock
+    Feline feline;
+    @Test
+    public void eatMeatShouldReturnPredatorFood() throws Exception {
+        List<String> expected = List.of("Животные", "Птицы", "Рыба");
+        Mockito.when(feline.eatMeat()).thenReturn(expected);
+        Feline feline1 = new Feline();
+        List<String> actual = feline1.eatMeat();
+        assertEquals(expected, actual);
+    }
+
 }
