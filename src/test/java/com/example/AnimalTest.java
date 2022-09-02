0fc3a01c353b825555672d42e50ed6a7c4da963b
Petr Merkurev <9662196@mail.ru>
2022-09-02 16:43:02 +0300

    AnimalTest added

--- src/test/java/com/example/AnimalTest.java
+++ src/test/java/com/example/AnimalTest.java
@@ -1,16 +1,25 @@
 package com.example;
 
+import org.junit.Assert;
 import org.junit.Test;
 
-import static org.junit.Assert.*;
 
 public class AnimalTest {
 
+    Animal animal;
+
     @Test
     public void getFood() {
+        animal = new Animal();
+    //    animal.getFood("Травоядное");
     }
 
     @Test
     public void getFamily() {
+        animal = new Animal();
+        String expected = "Существует несколько семейств: заячьи, беличьи, мышиные, кошачьи, псовые, медвежьи, куньи";
+        String actual = animal.getFamily();
+        Assert.assertEquals(expected, actual);
+
     }
 }
